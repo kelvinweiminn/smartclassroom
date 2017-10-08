c6923ddc55c91a75050cdb380fe3efaced00ce2d
Kelvin Wei Minn <mg.weiminn@gmail.com>
2017-10-08 15:19:31 +0800

    CORS enabled

--- backend-actuation/index.js
+++ backend-actuation/index.js
@@ -4,6 +4,13 @@ var mysql = require('mysql');
 var moment = require('moment-timezone');
 var request = require("request");
 
+//middle
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var bodyparser = require('body-parser');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
--- backend/script.js
+++ backend/script.js
@@ -4,6 +4,18 @@ var bodyparser = require('body-parser');
 var cors = require('cors');
 var path = require('path');
 
+//middle
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 const route = require('./routes/route.js');
 app.use('/api', route);
 const facedata = require('./routes/facedata.js');
@@ -11,12 +23,6 @@ app.use('/api/facedata', facedata);
 // const mockdata = require('./routes/mockdata.js');
 // app.use('/api/mockdata', mockdata);
 
-//middle
-app.use(cors);
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
 
 app.get('/', function(req, res){
   res.send("Invalid endpoint");
